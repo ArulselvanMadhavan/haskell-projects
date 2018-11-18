27995b4dca8e3980255cdd90faeacbfab56b36c8
Arulselvan Madhavan <arulselvan1234@gmail.com>
2018-11-17 20:03:40 -0800

    Finished Ch 2.1

---
+++ thinking-with-types/lib/Chapter2/Ex2_1_3_all.hs
@@ -0,0 +1,13 @@
+-- module Ex2_1_3_all where
+
+-- Ex2_1_3_i
+-- Kind of Show is * -> Constraint
+
+--Ex2_1_3_ii
+-- Kind of functor is (* -> *) -> Constraint
+
+--Ex2_1_3_iii
+-- Kind of Monad is (* -> *) -> Constraint
+
+--Ex2_1_4_iv
+-- Kind of MonadTrans class is ((* -> *) -> * -> *) -> Constraint
