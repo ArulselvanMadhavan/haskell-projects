746bb279fadb2c0cd94304adf9beeaebdf1b10c1
Arulselvan Madhavan <arulselvan1234@gmail.com>
2019-01-17 06:21:53 -0800

    Starting the Parallel and concurrent programming book

---
+++ thinking-with-types/lib/Chapter11/OpenProduct.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE AllowAmbiguousTypes   #-}
+{-# LANGUAGE ConstraintKinds       #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE KindSignatures        #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedLabels      #-}
+{-# LANGUAGE PolyKinds             #-}
+{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE UndecidableInstances  #-}
+
+import           Data.Kind            (Constraint, Type)
+import           Data.Proxy           (Proxy (..))
+import qualified Data.Vector          as V
+import           GHC.OverloadedLabels (IsLabel (..))
