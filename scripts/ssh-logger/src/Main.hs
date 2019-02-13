8252268f9dc216fcb5b431213ebaf4990860b89e
Arul Madhavan <arul.madhavan@arulmadhavan-MBP-3876.local>
2019-02-13 09:52:13 -0800

    Can read json files now

--- scripts/ssh-logger/src/Main.hs
+++ scripts/ssh-logger/src/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
@@ -5,13 +7,28 @@ import           Control.Applicative
 import qualified Control.Exception         as Exception
 import           Control.Monad.Error.Class
 import           Control.Monad.Except
+import           Data.Aeson
 import           Data.Bifunctor
+import           Data.ByteString.Lazy      as B
 import qualified Data.Either               as E
 import           Data.Either.Utils
 import           Data.List                 as L
 import           Data.Text                 as T
 import qualified Data.Text.IO              as TIO
+import           GHC.Generics
 import           Turtle
+
+
+data Config = Config {
+  branchName::Text
+  } deriving(Generic, FromJSON, ToJSON, Show)
+
+
+jsonFile :: Prelude.FilePath
+jsonFile = "config.json"
+
+getJSON :: IO B.ByteString
+getJSON = B.readFile jsonFile
 
 constructHostName :: Text -> Text
 constructHostName ipAddress =
@@ -20,7 +37,7 @@ constructHostName ipAddress =
 getHomeDirectory :: IO Text
 getHomeDirectory = do
   homeFP <- home
-  liftEither $ first (userError . unpack) (toText homeFP)
+  liftEither $ first (userError . T.unpack) (toText homeFP)
 
 data GithubCredentials = GithubCredentials { userName:: Text, password::Text } deriving (Show)
 
@@ -58,6 +75,8 @@ constructCloneCommand gc = T.concat ["git ", "clone ", constructCloneUrl gc]
 
 main :: IO ()
 main = do
+  d <- (eitherDecode <$> getJSON) :: IO (Either String Config)
+  Prelude.putStrLn $ show d
   TIO.putStrLn "Enter the ip address to connect:"
   ipaddress <- TIO.getLine
   homeDir <- getHomeDirectory
@@ -65,6 +84,8 @@ main = do
   TIO.putStrLn $ T.concat ["Copying bash script to ", ipaddress]
   gc <- buildGithubCredentials
   stdout $ inproc "ssh" ["-oStrictHostKeyChecking=no", ipaddress, constructCloneCommand gc] Turtle.empty
+  stdout $ inproc "ssh" ["-oStrictHostKeyChecking=no", ipaddress, "cd ~/adserver && sbt exit"] Turtle.empty
+  stdout $ inproc "ssh" ["-oStrictHostKeyChecking=no", ipaddress, "cd ~/adserver && sbt \";project adserver-exchange; debian:packageBin\""] Turtle.empty
   TIO.putStrLn "All set!!!"
 
 
