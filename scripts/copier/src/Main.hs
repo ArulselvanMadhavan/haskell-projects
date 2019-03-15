ad0ac88d0cc43322866178b8c7976ead54405c47
Arul Madhavan <arul.madhavan@arulmadhavan-MBP-3876.local>
2019-03-14 17:50:18 -0700

    First commit for copier

---
+++ scripts/copier/src/Main.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import           Control.Lens
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.AWS
+import           Data.ByteString.Builder (hPutBuilder)
+import           Data.Conduit
+import qualified Data.Conduit.List       as CL
+import           Data.Monoid
+import           Network.AWS.Data
+import           Network.AWS.EC2
+import           System.IO
+
+instanceOverview :: Region -> IO ()
+instanceOverview r = do
+  lgr <- newLogger Info stdout
+  env <- newEnv Discover <&> set envLogger lgr
+
+  let pp x = mconcat
+        [ "[instance:" <> build (x ^. insInstanceId) <> "] {"
+        , "\n  public-dns = " <> build (x ^. insPublicDNSName)
+        , "\n  tags       = " <> build (x ^. insTags . to show)
+        , "\n  state      = " <> build (x ^. insState . isName . to toBS)
+        , "\n}\n"
+        ]
+  runResourceT
+    .  runAWST env
+    .  within r
+    $  runConduit
+    $  paginate describeInstances
+    .| CL.concatMap (view dirsReservations)
+    .| CL.concatMap (view rInstances)
+    .| CL.mapM_ (liftIO . hPutBuilder stdout . pp)
+
+main :: IO ()
+main = do
+  putStrLn "Hello World"
+  instanceOverview NorthVirginia
